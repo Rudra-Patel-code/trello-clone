4e4b1037aa014da5ee00780afe64f5f40e324190
Rudra Patel <rudracode2004@gmail.com>
2023-12-14 09:06:25 +0530

    initial commir

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "./_component/footer";
+import Navbar from "./_component/navbar";
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full bg-slate-100">
+      <Navbar />
+      <main className="pt-40 pb-20 bg-slate-200">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MarketingLayout;
